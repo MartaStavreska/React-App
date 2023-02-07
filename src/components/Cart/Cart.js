d815b49fbbf22933edd184bee24ffdb306317618
MartaStavreska <stavreskamarta@gmail.com>
2023-02-07 15:15:09 +0100

    test

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -6,6 +6,7 @@ import CartContext from "../../store/cart-context";
 import CartItem from "./CartItem";
 const Cart = (props) => {
   const cartCtx = useContext(CartContext);
+  console.log("test");
 
   const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
   const hasItems = cartCtx.items.length > 0;
